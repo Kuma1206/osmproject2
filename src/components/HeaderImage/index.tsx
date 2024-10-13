03c3a1ea639b8b5654f595ac29eb6d3966ccb016
Kuma1206 <tadakuma369@gmail.com>
2024-10-13 13:41:49 +0900

    ヘッダー動画を追加

--- src/components/HeaderImage/index.tsx
+++ src/components/HeaderImage/index.tsx
@@ -2,7 +2,18 @@ import React from "react";
 import styles from "./style.module.scss";
 
 const HeaderImage = () => {
-  return <div className={styles.headerimage}>ヘッダー画像</div>;
+  return (
+    <div className={styles.headerimage}>
+      <video className={styles.video} autoPlay muted loop>
+        <source src="/videos/ヘッダー動画.mp4" type="video/mp4" />
+      </video>
+      <p className={styles.title}>
+        日本の<br/>
+        アマチュアクリエイターで
+        世界を変える
+      </p>
+    </div>
+  );
 };
 
 export default HeaderImage;
